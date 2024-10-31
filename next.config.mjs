09b6ad7ceef5d8a854c547509da6e74a6c405ff0
04041b <04041b@ceabq.top>
2024-10-30 23:08:36 -0700

    build rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    //image optimization not available without next server :(
     images: { unoptimized: true }
 };
 
